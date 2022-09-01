22a6ff558329af8553531831938af383a6a87146
Fenrisulfr.33 <Fenrisulfr.33@protonmail.com>
2022-08-31 20:41:55 -0400

    added 06 pokedex that fixed the egg move problem

--- dataFetcher.js
+++ dataFetcher.js
@@ -15,15 +15,18 @@ function dataFetcher(
     oldData = [],
     fileSaveURL = '',
     converterFunc,
-    errors = {},
+    returnObj = true,
 ) {
     let index = 0;
-    const newData = [];
-    const fetcher = async () => {
+    const newData = [],
+    errors = {},
+    checkObj = {},
+    fetcher = async () => {
         try {
             if (index > lastNum) {
                 // convert JSON object to string
-                const data_array = JSON.stringify(newData, null, 2); // this makes it pretty
+                const sortedArray = newData.sort((a, b) => a._id - b._id);
+                const data_array = JSON.stringify(sortedArray, null, 2); // this makes it pretty
     
                 // write JSON string to a file
                 fs.writeFile(fileSaveURL, data_array, (error) => {
@@ -31,20 +34,35 @@ function dataFetcher(
                         console.log(error);
                     }
                     console.log("JSON data is saved.");
+                    console.log('CheckObj', checkObj);
                     console.log('Errors:', errors);
                 });
                 return;
             }
             // if changes are being made locally without api request 
             if (dataURL === ''){
-                converterFunc(oldData[index], {}, newData);
+                if (returnObj) { // if it is a object being returned
+                    const returnObj = converterFunc(oldData[index], {}, newData, errors, checkObj);
+                    newData.push(returnObj);
+                } else { // it is an array being returned
+                    const returnArray = converterFunc(oldData[index], {}, newData, errors, checkObj);
+                    if (returnArray.length > 0) {
+                        returnArray.forEach((item) => {newData.push(item)});
+                    }
+                }
             } else {
                 const response = await fetch(`${dataURL}/${index + 1}`);
                 const data = await response.json();
                 if (oldData.length === 0) {
-                    converterFunc(data, newData, null, errors);
+                    if (returnObj){
+                        const returnObj = converterFunc(data, newData, null, errors, checkObj);
+                        newData.push(returnObj);
+                    }
                 } else {
-                    converterFunc(oldData[index], data, newData, errors);
+                    if (returnObj) {
+                        const returnObj = converterFunc(oldData[index], data, newData, errors, checkObj);
+                        newData.push(returnObj);
+                    }
                 }
             }
             index++;
--- pokedex_rewrites/05-pokedex_rewrite.js
+++ pokedex_rewrites/05-pokedex_rewrite.js
@@ -1,8 +1,9 @@
 /**
  * Placeholder from manual rewrite due to lack of API info on last 810-899 pokemon
- */const dataFetcher = require('../dataFetcher')
+ */
+const dataFetcher = require('../dataFetcher')
 const pokedex = require('../04-jsons/04-pokedex.json');
-const evolutions = require('../evolutions/evolutionAssigning.json');
+const evolutions = require('../evolutions/00-evolutionsAssigning.json');
 /**
  * MAIN FUNCTION
  * 
@@ -14,12 +15,32 @@ const evolutions = require('../evolutions/evolutionAssigning.json');
 const pokedexRewrite05 = (old_data, new_data, new_array) => {   
     // Give the first 151 evolution numbers and find a way to transfer all know egg moves from the lowest id available
     if (old_data._id === evolutions[old_data._id - 1].id) {
-        old_data.evolution = evolutions[old_data._id - 1].evolution;
+        old_data.evolution = {};
+        old_data.evolution.id = evolutions[old_data._id - 1].evolution;
+        old_data.evolution.start = evolutions[old_data._id - 1].start ? true : null
     } else {
         console.log(`Failed at pokedex number: ${old_data._id}`);
     }
+    // Add Generations for checking
+    if (old_data._id >= 1 && old_data._id < 152){
+        old_data['generation'] = 1;
+    } else if (old_data._id >= 152 && old_data._id < 252){
+        old_data['generation'] = 2;
+    } else if (old_data._id >= 252 && old_data._id < 387){
+        old_data['generation'] = 3;
+    } else if (old_data._id >= 387 && old_data._id < 494){
+        old_data['generation'] = 4;
+    } else if (old_data._id >= 494 && old_data._id < 650){
+        old_data['generation'] = 5;
+    } else if (old_data._id >= 650 && old_data._id < 722){
+        old_data['generation'] = 6;
+    } else if (old_data._id >= 722 && old_data._id < 810){
+        old_data['generation'] = 7;
+    } else if (old_data._id >= 810 && old_data._id < 899){
+        old_data['generation'] = 8;
+    } 
 
-    new_array.push(old_data) // send your new data into the final product
+    return old_data;// send your new data into the final product
 }
 
 /**
@@ -32,7 +53,7 @@ const pokedexRewrite05 = (old_data, new_data, new_array) => {
  */
 dataFetcher(
     '', // no API needed
-    385, // includes gen 3 and down
+    897, // includes gen 3 and down
     pokedex, // old data
     "../05-jsons/05-pokedex.json", // new file write
     pokedexRewrite05, // function passed in
