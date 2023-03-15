975365690406a98a1b1747ba587bda543680355a
Archer <fenrisulfr.33@protonmail.com>
2023-03-15 12:39:18 -0600

    added new pokemon

---
+++ pokemon/data/forms/paldeanForms/walking-wake.js
@@ -0,0 +1,129 @@
+/**
+ * 03-14-2023 Verfied data is correct - Archer
+ */
+const walkingWake = {
+    _id: 1009,
+    name: {
+      english: "Walking Wake",
+    },
+    pokedexNumber: {
+      scvi: 401,
+    },
+    type: {
+      one: "Water",
+      two: "Dragon",
+    },
+    abilities: {
+      one: {
+        name: "Protosynthesis",
+        id: 281,
+      }
+    },
+    baseStats: {
+      hp: 99,
+      atk: 83,
+      def: 91,
+      spatk: 125,
+      spdef: 83,
+      spd: 109,
+      total: 590,
+    },
+    info: {
+      height: "3.5 m (11′06″)",
+      weight: "280.0 kg (617.3 lbs)",
+    },
+    gender: {
+      male: "",
+      female: "",
+      genderless: true,
+    },
+    evolution: null,
+    generation: 9,
+    evs: {},
+    eggGroups: ['Undiscovered'],
+    species: "	Paradox Pokémon",
+    catchRate: 0,
+    baseFriendship: 0,
+    baseExp: 0,
+    growthRate: "",
+    eggCycles: 0,
+    nameOrigin: {},
+    gameDropDown: [
+      {
+        game: "Scarlet & Violet",
+        query: "scarlet-violet",
+      },
+    ],
+    pokedexEntries: {},
+    whereToFind: [
+        {
+            game: 'Scarlet/Violet',
+            location: [
+                'Tera Raid Den Event'
+            ]
+        }
+    ],
+    moves: {
+      "level-up": [
+        { name: "Aqua Jet", lvl: 1 },
+        { name: "Leer", lvl: 1 },
+        { name: "Roar", lvl: 1 },
+        { name: "Twister", lvl: 1 },
+        { name: "Bite", lvl: 7 },
+        { name: "Water Pulse", lvl: 14 },
+        { name: "Noble Roar", lvl: 21 },
+        { name: "Dragon Breath", lvl: 28 },
+        { name: "Breaking Swipe", lvl: 35 },
+        { name: "Dragon Rush", lvl: 42 },
+        { name: "Hydro Steam", lvl: 56 },
+        { name: "Dragon Pulse", lvl: 62 },
+        { name: "Outrage", lvl: 70 },
+        { name: "Flamethrower", lvl: 77 },
+        { name: "Hydro Pymp", lvl: 84 },
+
+      ],
+      remember: [
+        "Hone Claws",
+        "Sunny Day"
+      ],
+      machine: [
+        "Take Down",
+        "Agility",
+        "Scary Face",
+        "Protect",
+        "Fire Fang",
+        "Water Pulse",
+        "Low Kick",
+        "Chiling Water",
+        "Facade",
+        "Snarl",
+        "Swift",
+        "Mud Shot",
+        "Dragon Tail",
+        "Endure",
+        "Sunny Day",
+        "Rain Dance",
+        "Body Slam",
+        "Sleep Talk",
+        "Waterfall",
+        "Dragon Claw",
+        "Rest",
+        "Dragon Dance",
+        "Substitute",
+        "Crunch",
+        "Liquidation",
+        "Dragon Pulse",
+        "Surf",
+        "Flamethrower",
+        "Hydro Pump",
+        "Giga Impact",
+        "Outrage",
+        "Hurricane",
+        "Hyper Beam",
+        "Draco Meteor",
+        "Tera Blast",
+      ]
+    },
+  };
+  module.exports = walkingWake;
+  
