ae06f37adf184bcdd43d0a642612cfac7876c8ed
Fenrisulfr.33 <Fenrisulfr.33@protonmail.com>
2023-04-03 02:21:36 -0400

    started temtem comp profile

---
+++ temtem/comp-temtem-season3.js
@@ -0,0 +1,64 @@
+const temtemCompPatch1Point3 = [
+    {
+        _id: 163,
+        tier: 'S',
+        name: 'Tyranak',
+        ability: 'Intimidator',
+        heldItem: 'Bait',
+        techniques: [
+            'Embers',
+            'Meteror Swarm',
+            'Outgrowth',
+            'Flaming Meteorite'
+        ],
+        baseStats: {
+            hp: 78,
+            sta: 48,
+            spd: 34,
+            atk: 81,
+            def: 74,
+            spAtk: 57,
+            spDef: 76,
+        },
+        tvs: {
+            hp: 500,
+            sta: 0,
+            spd: 1,
+            atk: 497,
+            def: 2,
+            spAtk: 0,
+            spDef: 0,
+        }
+    },
+    {
+        _id: 12,
+        tier: 'S',
+        name: 'Tateru',
+        ability: 'Soft Touch',
+        heldItem: 'Sweatband',
+        techniques: [
+            'Held Anger',
+            'Perfect Jab',
+            'Major Slash',
+            'Stone Ball'
+        ],
+        baseStats: {
+            hp: 79,
+            sta: 85,
+            spd: 60,
+            atk: 78,
+            def: 66,
+            spAtk: 54,
+            spDef: 66,
+        },
+        tvs: {
+            hp: 459,
+            sta: 44,
+            spd: 0,
+            atk: 497,
+            def: 0,
+            spAtk: 0,
+            spDef: 0,
+        }
+    },
+]
