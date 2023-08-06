a46cc416fe41b929950f1a2e60931fde1de027c3
Fenrisulfr.33 <Fenrisulfr.33@protonmail.com>
2023-08-06 19:49:14 -0400

    added usum dex, and 2 regions, working on formChange

---
+++ pokemon/helperFunctions/addMovesToForms.js
@@ -0,0 +1,10 @@
+function addMovesToForms(orginalPokemon, formPokemon){
+    const returnFormPokemon = { ...formPokemon };
+    formPokemon.movesToCopy.forEach((game) => {
+
+    })
+}
+
+module.exports = {
+    addMovesToForms,
+}
