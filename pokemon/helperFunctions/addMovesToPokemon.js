434a5a3c203a164241cc46000a3443378ea2d79d
Archer <fenrisulfr.33@protonmail.com>
2023-08-08 08:56:39 -0400

    working on adding moves to all mons

--- pokemon/helperFunctions/addMovesToPokemon.js
+++ pokemon/helperFunctions/addMovesToPokemon.js
@@ -1,52 +1,41 @@
-const bdspMoves = require("../webscraping/00-bdsp-moves.json");
-const laMoves = require("../webscraping/00-legends-arceus-moves.json");
-const swshMoves = require("../webscraping/00-sword-shield-moves.json");
+const redBlueMoves = require("../data/moves/01-red-blue-moves.json");
+const yellowMoves = require("../data/moves/01-yellow-moves.json");
+const goldSilverMoves = require('../data/moves/02-gold-silver-moves.json');
+const crystalMoves = require('../data/moves/02-crystal-moves.json');
+const rubySapphireMoves = require('../data/moves/03-ruby-sapphire-moves.json');
+const emeraldMoves = require('../data/moves/03-emerald-moves.json');
+const fireRedLeafGreenMoves = require('../data/moves/03-firered-leafgreen-moves.json');
+const diamondPearlMoves = require('../data/moves/04-diamond-pearl-moves.json');
+const platinumMoves = require('../data/moves/04-platinum-moves.json');
+
+const movesByGame = [
+  { moves: redBlueMoves, key: 'red-blue', length: 151 },
+  { moves: yellowMoves, key: 'yellow', length: 151 },
+  { moves: goldSilverMoves, key: 'gold-silver', length: 251 },
+  { moves: crystalMoves, key: 'crystal', length: 251 },
+  { moves: rubySapphireMoves, key: 'ruby-sapphire', length: 386 },
+  { moves: emeraldMoves, key: 'emerald', length: 386 },
+  { moves: fireRedLeafGreenMoves, key: 'fire-red-leaf-green', length: 386 },
+  { moves: diamondPearlMoves, key: 'diamond-pearl', length: 493 },
+  { moves: platinumMoves, key: 'platinum', length: 493 },
+]
+
+const writeMoves = (moves, monId, returnObj, game, length) => {
+  if (monId <= length) {
+    const foundMoves = moves.find((mon) => mon.id === monId);
+    if (foundMoves) {
+      returnObj[game] = foundMoves[game];
+    }
+  }
+  return returnObj;
+}
 
 function addMovesToPokemon(pokemonId, pokemonMoves) {
   // Create a copy of the pokemon for return.
-  const returnMoves = { ...pokemonMoves };
-
-  // If the pokemon id is above id 493, skip this step, as BDSP can only have the first 493.
-  if (pokemonId <= 898) {
-    // Find is fast here because it is a sorted array.
-    const foundSwshMoves = swshMoves.find(
-      (swshMon) => swshMon.id === pokemonId
-    );
-    // If the object exist which it should.
-    if (foundSwshMoves) {
-      // Give the old object the new moves object.
-      returnMoves["sword-shield"] = foundSwshMoves["sword-shield"];
-    }
-  }
-
-  // LEGENDS ARCEUS ADDITION
-  // If the pokemon id is above id 493, skip this step, as BDSP can only have the first 493.
-  if (pokemonId <= 905) {
-    // Find is fast here because it is a sorted array.
-    const foundLaMoves = laMoves.find((laMon) => laMon.id === pokemonId);
-    // If the object exist.
-    if (foundLaMoves) {
-      // Give the old object the new moves object.
-      returnMoves["legends-arceus"] = foundLaMoves["legends-arceus"];
-    }
-  }
-
-  // BDSP MOVE ADDITION
-
-  // If the pokemon id is above id 493, skip this step, as BDSP can only have the first 493.
-  if (pokemonId <= 493) {
-    // Find is fast here because it is a sorted array.
-    const foundBdspMoves = bdspMoves.find(
-      (bdspMon) => bdspMon.id === pokemonId
-    );
-    // If the object exist which it should.
-    if (foundBdspMoves) {
-      // Give the old object the new moves object.
-      returnMoves["brilliant-diamond-shining-pearl"] =
-        foundBdspMoves["brilliant-diamond-shining-pearl"];
-    }
-  }
-
+  let returnMoves = { ...pokemonMoves };
+  movesByGame.forEach((game) => {
+    returnMoves = writeMoves(game.moves, pokemonId, returnMoves, game.key, game.length)
+  });
   return returnMoves;
 }
 
