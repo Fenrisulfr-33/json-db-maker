723e939ffa201f65928a457c51e22f443f1753bf
Archer <fenrisulfr.33@protonmail.com>
2024-01-28 20:35:10 -0500

    Palworld data join, upsert, and mongodb connection made

---
+++ palworld/paldex-join.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const paldexPath = require("path").join(__dirname, "./paldex");
+const paldexJoined = [];
+const errors = {};
+
+require("fs")
+  .readdirSync(paldexPath)
+  .forEach(function (file) {
+    const pal = require(`./paldex/${file}`);
+    // TODO: Add a returnPalModel() function so it always returns the format we want it
+    paldexJoined.push(pal);
+  });
+// Sort paldex before saving.
+const paldexSorted = paldexJoined.sort((palA, palB) => {
+  if (palA._id < palB._id) {
+    return -1;
+  }
+});
+
+const saveData = JSON.stringify(paldexSorted, null, 2);
+
+console.log("Paldex length:", paldexSorted.length);
+if (Object.keys(errors).length > 0) {
+  console.log("errors", errors);
+} else {
+  console.log("No errors");
+}
+// Write JSON string to a file
+fs.writeFile(
+  `./${new Date().toJSON().slice(0, 10)}-paldex.json`,
+  saveData,
+  (error) => {
+    error ? console.error(error) : null;
+    console.log(`${new Date().toJSON().slice(0, 10)}-paldex.json saved.`);
+  }
+);
---
+++ palworld/paldex-upsert.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require("mongodb");
+const paldex = require("./2024-01-29-paldex.json");
+const { PALWOLRD_URI } = require("./CONSTANTS");
+const client = new MongoClient(PALWOLRD_URI);
+
+const replaceInsertPalToDB = async () => {
+  for (let i = 0; i < paldex.length; i++) {
+    const pal = paldex[i];
+    await replaceDocument(pal);
+  }
+  await client.close();
+};
+
+async function replaceDocument(replacementNewPal) {
+  try {
+    // Get the database and collection on which to run the operation
+    const palworld = client.db("palworld");
+    const paldex = palworld.collection("pal-dex");
+    // Create a query for documents where the title contains "The Cat from"
+    const query = { _id: replacementNewPal._id };
+    const options = {
+      upsert: true,
+    };
+    // Execute the replace operation
+    const result = await paldex.replaceOne(query, replacementNewPal, options);
+    if (result.modifiedCount > 0) {
+      console.log(
+        `Modified ${result.modifiedCount} document(s) at position ${replacementNewPal._id}`
+      );
+    } else if (result.upsertedId) {
+      console.log(
+        `Inserted ${result.upsertedId} document at position ${replacementNewPal._id}`
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+replaceInsertPalToDB();
