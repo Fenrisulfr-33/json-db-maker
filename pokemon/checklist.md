9107628ca00ec4166ab394f9d495fef02a2b3898
Gin Silver <Fenrisulfr.33@protonmail.com>
2024-02-12 13:00:49 -0500

    alolan pokemon added and formTab updated

---
+++ pokemon/checklist.md
@@ -0,0 +1,25 @@
+
+# Pokemon DB Checklist
+
+This document is for management and assurances of data in these documents.
+
+| pokemon           | .json | sm-moves  | usum-moves    | lets-go   | formTab   |
+| -                 | -     | -         | -             | -         | -         |
+| Alolan Rattata    | x     | x         | -             | -         | x         |
+| Alolan Raticate   | x     | x         | -             | -         | x         |
+| Alolan Raichu     | x     | -         | -             | -         | x         |
+| Alolan Sandshrew  | x     | -         | -             | -         | x         |
+| Alolan Sandslash  | x     | -         | -             | -         | x         |
+| Alolan Vulpix     | x     | -         | -             | -         | x         |
+| Alolan Ninetales  | x     | -         | -             | -         | x         |
+| Alolan Diglett    | x     | -         | -             | -         | x         |
+| Alolan Dugtrio    | x     | -         | -             | -         | x         |
+| Alolan Meowth     | x     | -         | -             | -         | x         |
+| Alolan Persian    | x     | -         | -             | -         | x         |
+| Alolan Geodude    | x     | -         | -             | -         | x         |
+| Alolan Graveler   | x     | -         | -             | -         | x         |
+| Alolan Golem      | x     | -         | -             | -         | x         |
+| Alolan Grimer     | x     | -         | -             | -         | x         |
+| Alolan Muk        | x     | -         | -             | -         | x         |
+| Alolan Exeggutor  | x     | -         | -             | -         | x         |
+| Alolan Marowak    | x     | -         | -             | -         | x         |
--- pokemon/forms/formTabs.js
+++ pokemon/forms/formTabs.js
@@ -8,6 +8,170 @@
   // },
 
 const formTabs = [
+  // Sun & Moon
+  {
+    _id: 19,
+    pokemonIds: [ 19, 19.1 ],
+    key: 'rattata',
+    tab: [
+      { id: 19, name: 'Rattata' },
+      { id: 19.1, name: 'Alolan Rattata' },
+    ]
+  },
+  {
+    _id: 20,
+    pokemonIds: [ 20, 20.1 ],
+    key: 'raticate',
+    tab: [
+      { id: 20, name: 'Raticate' },
+      { id: 20.1, name: 'Alolan Raticate' },
+    ]
+  },
+  {
+    _id: 26,
+    pokemonIds: [ 26, 26.1 ],
+    key: 'raichu',
+    tab: [
+      { id: 26, name: 'Raichu' },
+      { id: 26.1, name: 'Alolan Raichu' },
+    ]
+  },
+  {
+    _id: 27,
+    pokemonIds: [ 27, 27.1 ],
+    key: 'sandshrew',
+    tab: [
+      { id: 27, name: 'Sandshrew' },
+      { id: 27.1, name: 'Alolan Sandshrew' },
+    ]
+  },
+  {
+    _id: 28,
+    pokemonIds: [ 28, 28.1 ],
+    key: 'sandslash',
+    tab: [
+      { id: 28, name: 'Sandslash' },
+      { id: 28.1, name: 'Alolan Sandslash' },
+    ]
+  },
+  {
+    _id: 37,
+    pokemonIds: [ 37, 37.1 ],
+    key: 'vulpix',
+    tab: [
+      { id: 37, name: 'Vulpix' },
+      { id: 37.1, name: 'Alolan Vulpix' },
+    ]
+  },
+  {
+    _id: 38,
+    pokemonIds: [ 38, 38.1 ],
+    key: 'ninetales',
+    tab: [
+      { id: 38, name: 'Ninetales' },
+      { id: 38.1, name: 'Alolan Ninetales' },
+    ]
+  },
+  {
+    _id: 50,
+    pokemonIds: [ 50, 50.1 ],
+    key: 'diglett',
+    tab: [
+      { id: 50, name: 'Diglett' },
+      { id: 50.1, name: 'Alolan Diglett' },
+    ]
+  },
+  {
+    _id: 51,
+    pokemonIds: [ 51, 51.1 ],
+    key: 'dugtrio',
+    tab: [
+      { id: 50, name: 'Dugtrio' },
+      { id: 50.1, name: 'Alolan Dugtrio' },
+    ]
+  },
+  {
+    _id: 52,
+    pokemonIds: [ 52, 52.1 ],
+    key: 'meowth',
+    tab: [
+      { id: 52, name: 'Meowth' },
+      { id: 52.1, name: 'Alolan Meowth' },
+    ]
+  },
+  {
+    _id: 53,
+    pokemonIds: [ 53, 53.1 ],
+    key: 'persian',
+    tab: [
+      { id: 53, name: 'Persian' },
+      { id: 53.1, name: 'Alolan Persian' },
+    ]
+  },
+  {
+    _id: 74,
+    pokemonIds: [ 74, 74.1 ],
+    key: 'geodude',
+    tab: [
+      { id: 74, name: 'Geodude' },
+      { id: 74.1, name: 'Alolan Geodude' },
+    ]
+  },
+  {
+    _id: 75,
+    pokemonIds: [ 75, 75.1 ],
+    key: 'graveler',
+    tab: [
+      { id: 75, name: 'Graveler' },
+      { id: 75.1, name: 'Alolan Graveler' },
+    ]
+  },
+  {
+    _id: 76,
+    pokemonIds: [ 76, 76.1 ],
+    key: 'golem',
+    tab: [
+      { id: 76, name: 'Golem' },
+      { id: 76.1, name: 'Alolan Golem' },
+    ]
+  },
+  {
+    _id: 88,
+    pokemonIds: [ 88, 88.1 ],
+    key: 'grimer',
+    tab: [
+      { id: 88, name: 'Grimer' },
+      { id: 88.1, name: 'Alolan Grimer' },
+    ]
+  },
+  {
+    _id: 89,
+    pokemonIds: [ 89, 89.1 ],
+    key: 'muk',
+    tab: [
+      { id: 89, name: 'Muk' },
+      { id: 89.1, name: 'Alolan Muk' },
+    ]
+  },
+  {
+    _id: 103,
+    pokemonIds: [ 103, 103.1 ],
+    key: 'exeggutor',
+    tab: [
+      { id: 103, name: 'Exeggutor' },
+      { id: 103.1, name: 'Alolan Exeggutor' },
+    ]
+  },
+  {
+    _id: 105,
+    pokemonIds: [ 105, 105.1 ],
+    key: 'marowak',
+    tab: [
+      { id: 105, name: 'Marowak' },
+      { id: 105.1, name: 'Alolan Marowak' },
+    ]
+  },
+  // Sword & Shield
   {
     _id: 898,
     pokemonIds: [ 898, 898.1, 898.2 ],
